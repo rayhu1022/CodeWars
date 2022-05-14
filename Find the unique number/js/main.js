416d0d0e1bc705ac6926ca8dd7f53eb219011d68
Ray Hu <ysrayhu@gmail.com>
2022-05-13 22:05:35 -0700

    Code wars May 13, 2022. 6 kyu

---
+++ Find the unique number/js/main.js
@@ -0,0 +1,13 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+    let newArr = arr.sort((a,b) => a - b)
+    return newArr[0] === newArr[1] ? newArr[newArr.length - 1] : newArr[0]
+}
+  
