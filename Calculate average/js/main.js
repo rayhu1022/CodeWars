fef84614397fea21b23b24c56d386ca47d4fb08e
Ray Hu <ysrayhu@gmail.com>
2022-05-28 17:23:10 -0700

    Code wars May 28, 2022. 6 kyu

--- Calculate average/js/main.js
+++ Calculate average/js/main.js
@@ -3,5 +3,8 @@
 // Note: Empty arrays should return 0.
 
 function find_average(array) {
-    return array.reduce((accum, number) => accum + number, 0) / array.length;
+    if (array.length > 0) {
+        return array.reduce((accum, number) => accum + number, 0) / array.length;
+    }
+    return 0
 }
